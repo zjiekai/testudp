547f2bf59f1b8b8b4c28db7008a3e65a029f43c2
Jiekai <jiekai.zheng@transwarp.io>
2016-05-17 16:08:12 +0800

    local remote ping pong

--- udp.go
+++ udp.go
@@ -9,18 +9,44 @@ import (
 
 var (
 	laddr = flag.String("l", "0.0.0.0:8060", "local address")
+	raddr = flag.String("r", "", "remote address")
 )
 
+func heartbeat(c net.PacketConn, addr net.Addr) {
+	const beatInterval = 3 * time.Second
+	ticker := time.NewTicker(beatInterval)
+	go func() {
+		for {
+			<-ticker.C
+			n, err := c.WriteTo([]byte("zjk"), addr)
+			if err != nil {
+				log.Print(err)
+			} else {
+				log.Printf("send %d\n", n)
+			}
+		}
+	}()
+}
+
 func Start() {
-	c, err := net.ListenPacket("udp", *laddr)
+	c, err := net.ListenPacket("udp4", *laddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer c.Close()
 
+	if len(*raddr) > 0 {
+		paddr, err := net.ResolveUDPAddr("udp", *raddr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("remote: %v\n", *paddr)
+		heartbeat(c, paddr)
+	}
+
 	log.Printf("%v\n", c.LocalAddr())
 	for {
-		if err := c.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+		if err := c.SetReadDeadline(time.Now().Add(10 * time.Second)); err != nil {
 			log.Fatal(err)
 		}
 
@@ -29,8 +55,9 @@ func Start() {
 		n, addr, err := c.ReadFrom(rb)
 		if err != nil {
 			log.Print(err)
+		} else {
+			log.Printf("%d %v\n", n, addr)
 		}
-		log.Printf("%d %v\n", n, addr)
 	}
 }
 
